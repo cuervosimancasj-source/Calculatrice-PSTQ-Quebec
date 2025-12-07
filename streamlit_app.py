6ff7952db47c03e9b18456f84c0b09f7fb56604a
cuervosimancasj-source <cuervosimancasj@gmail.com>
2025-12-07 18:26:38 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,16 +14,15 @@ st.set_page_config(
 # -----------------------------------------------------
 
 GA_CODE = """
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-YGX9B3XW84"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  // ⚠️ REEMPLAZA G-T0CE5LQHVF con tu ID real de GA4
-  gtag('config', 'G-YGX9B3XW84'); 
+<script type="text/javascript">
+    (function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "ui0x26pteb");
 </script>
-"""
+
+  """
 
 def inject_ga():
     # Inyecta el código HTML/JavaScript en la página
